b8e8bbd6de90066d8139d038e0767976ed9a4785
isabelle.amsalu@gmail.com <isabelle.amsalu@gmail.com>
2025-10-25 16:22:03 +0200

    advanced routing

--- routes/users.js
+++ routes/users.js
@@ -9,5 +9,18 @@ router.get("/new", (req, res) => {
   res.send("User New Form");
 });
 
+//Create new user
+router.post("/", (req, res) => {
+  res.send("Create user");
+});
+
+//access user based on id of user
+//route dynamically determined by the url
+router.get("/:id", (req, res) => {
+  //access parameter
+  req.params.id;
+  res.send(`Get user with id ${req.params.id}`);
+});
+
 //export router
 module.exports = router;
